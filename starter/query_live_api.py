4a96feaa2e391bb555cd87588b4ab623fb60c76a
clopezh <carlos.lopez@rimac.com.pe>
2023-01-04 18:49:04 -0500

    feat: add query of live api

---
+++ starter/query_live_api.py
@@ -0,0 +1,23 @@
+import requests
+
+url = "https://scalable-fastapi-model.herokuapp.com/predict/"
+features = {
+    "age": 59,
+    "workclass": "Private",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education_num": 13,
+    "marital_status": "Divorced",
+    "occupation": "Adm-clerical",
+    "relationship": "Wife",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 12174,
+    "capital_loss": 0,
+    "hours_per_week": 40,
+    "native_country": "United-States"
+}
+
+response = requests.post(url, json=features)
+print(response.text)
+
